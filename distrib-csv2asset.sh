554783a29b7abf6470195a4e5c4e4ac71a455447
Jaime MADRID <jaime.madrid@akeneo.com>
2020-04-21 17:10:10 +0200

    Delete distrib-csv2asset.sh

--- distrib-csv2asset.sh
+++
@@ -1,1 +0,0 @@
-tar cvfz ../CsvToAsset.tgz  --exclude='CsvToAsset/vendor/*' --exclude='CsvToAsset/var/*' --exclude='CsvToAsset/.git/*' --exclude='CsvToAsset/.idea' --exclude='CsvToAsset/..DS_Store' ../CsvToAsset
